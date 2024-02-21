d2f6d0ed8eb2cc36560b09ec80bf47c4f76cb48b
mattsidoti <159968372+mattsidoti@users.noreply.github.com>
2024-02-20 19:43:04 -0500

    Add files via upload

---
+++ 2-20-24.py
@@ -0,0 +1,15 @@
+
+date = input("Enter the date: MM/DD/YYYY")
+
+
+month = date[0:2]
+print(f'Month is {month}')
+
+day = date[3:5]
+print(f'Day is {day}')
+
+year = date[6:10]
+print(f'Year is {year}')
+
+
+
